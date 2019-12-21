71a5f1ce1e997877658dc839b071e91aaeee4893
Sourav Goswami <souravgoswami@protonmail.com>
2019-12-21 16:10:13 +0530

    Add files via upload

--- Project 6 - Our Lovely Course [HTML, CSS, JQuery, Bootstrap]/src/snow.js
+++ Project 6 - Our Lovely Course [HTML, CSS, JQuery, Bootstrap]/src/snow.js
@@ -1,11 +1,11 @@
 class SnowField {
 	constructor() {
-		this.fps = 40
-		this.canvas = null ;
+		this.fps = 30
+		this.canvas = null
 		this.width = 0
 		this.height = 0
-		this.stars = 200
-		this.intervalId = 0
+		this.snow = 200
+		this.snowParticles = []
 	}
 
 	initialize(canvas) {
@@ -18,10 +18,10 @@ class SnowField {
 			this.canvas.width = this.width
 			this.canvas.height = this.height
 
-			for (let i = 0 ; i < this.stars ; ++i)
-				this.stars[i] = new Snow(Math.random() * this.width, Math.random() * this.height, Math.random() * 2 + 1)
+			for (let i = 0 ; i < this.snow ; ++i)
+				this.snowParticles[i] = new Snow(Math.random() * this.width, Math.random() * this.height, Math.random() * 2 + 1)
 
-			this.draw()
+			this.update()
 		})
 
 		this.canvas = document.getElementById(canvas)
@@ -30,46 +30,35 @@ class SnowField {
 	}
 
 	start() {
-		var stars = []
-		for (let i = 0 ; i < this.stars ; ++i )
-			stars[i] = new Snow(Math.random() * this.width, Math.random() * this.height, Math.random() * 2 + 1)
+		for (let i = 0 ; i < this.snow ; ++i )
+			this.snowParticles[i] = new Snow(Math.random() * this.width, Math.random() * this.height)
 
-		this.stars = stars
-
-		this.intervalId = setInterval(() => {
-			this.update()
-			this.draw()
-		}, 1000 / this.fps)
+		setInterval(() => { this.update() }, 1000 / this.fps)
 	}
 
 	update() {
-		for (var i = 0 ; i < this.stars.length ; ++i) {
-			var star = this.stars[i]
-			star.x += Math.sin(i)
-			star.y += star.yspeed
-
-			if ((star.y > this.height) || (star.x < -star.size || star.x > this.width))
-				this.stars[i] = new Snow(Math.random() * this.width, 0, Math.random() * 3 + 1)
-		}
-	}
-
-	draw() {
 		var ctx = this.canvas.getContext('2d')
 		ctx.clearRect(0, 0, this.width, this.height)
 
-		for(let i = 0 ; i < this.stars.length ; ++i) {
-			var star = this.stars[i]
-			ctx.fillStyle = '#fff'
-			ctx.fillRect(star.x, star.y, star.size, star.size)
+		for (let i = 0 ; i < this.snow ; ++i) {
+			var snow = this.snowParticles[i]
+			snow.x += Math.sin(i)
+			snow.y += snow.yspeed
+
+			if ((snow.y > this.height) || (snow.x < -snow.size || snow.x > this.width))
+				this.snowParticles[i] = new Snow(Math.random() * this.width, 0)
+
+			ctx.fillStyle = `rgba(255, 255, 255, ${this.height / (snow.y * 2)})`
+			ctx.fillRect(snow.x, snow.y, snow.size, snow.size)
 		}
 	}
 }
 
 class Snow {
-	constructor(x, y, size) {
+	constructor(x, y) {
 		this.x = x
 		this.y = y
-		this.size = size
-		this.yspeed = Math.random() * 10 + 1
+		this.size = Math.random() * 2 + 1
+		this.yspeed = Math.random() * 5 + 1
 	}
 }
